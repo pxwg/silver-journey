77352b20129a37ef248c87b45d6dc77068b3c0f4
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2025-01-06 04:20:59 +0800

    better input

--- lua/util/math_autochange.lua
+++ lua/util/math_autochange.lua
@@ -7,21 +7,21 @@ local function switch_rime_math()
     if (tex.in_mathzone() == true or tex.in_tikz() == true) and rime_ls_active == true then
       if _G.rime_toggled == true then
         require("lsp.rime_2").toggle_rime()
-        vim.cmd("Copilot enable")
+        -- vim.cmd("Copilot enable")
         _G.rime_toggled = false
       end
       -- in the text but rime is not active(by hand), do nothing
     elseif _G.rime_ls_active == false then
-      vim.cmd("Copilot enable")
+      -- vim.cmd("Copilot enable")
       -- in the text but rime is active(by hand ), thus the configuration is for mathzone or table or tikz
     else
       if _G.rime_toggled == false then
         require("lsp.rime_2").toggle_rime()
-        vim.cmd("Copilot disable")
+        -- vim.cmd("Copilot disable")
         _G.rime_toggled = true
       end
       if _G.rime_ls_active and _G.rime_toggled then
-        vim.cmd("Copilot disable")
+        -- vim.cmd("Copilot disable")
       end
     end
   end
